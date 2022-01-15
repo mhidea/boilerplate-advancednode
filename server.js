512a48abc3c30ce9f97803d347fceb6c34c7e192
MohammadHassan Haghighifard <mhideadesign@gmail.com>
2022-01-15 11:46:51 +0330

    db

--- server.js
+++ server.js
@@ -26,21 +26,36 @@ app.use(passport.session());
 
 app.set('view engine', 'pug')
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
 
-passport.deserializeUser((id, done) => {
-  done(null, null);
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-  //   done(null, null);
-  // });
-});
 
-app.route('/').get((req, res) => {
-  res.render('pug/index.pug', { title: 'Hello', message: 'Please login' });
-});
+myDB(async client => {
 
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+    res.render('pug', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, null);
+    });
+  });
+
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
