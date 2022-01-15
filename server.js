c37f9400e45dbcf1911f11caaef0ab53a4b97cff
MohammadHassan Haghighifard <mhideadesign@gmail.com>
2022-01-15 11:09:34 +0330

    rwre

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(passport.initialize());
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   cookie: { secure: false }
 }));
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'pug')
 
