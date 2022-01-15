98a11fd97648779b08771dcf173fb7b09af06fe3
MohammadHassan Haghighifard <mhideadesign@gmail.com>
2022-01-15 16:24:16 +0330

    auth strategy

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
-
-
+const LocalStrategy = require('passport-local');
 const myDB = require('./connection');
 const ObjectID = require('mongodb').ObjectID;
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
 const app = express();
 
+
 fccTesting(app); //For FCC testing purposes
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,16 +23,26 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.set('view engine', 'pug')
 
 
 
 myDB(async client => {
-
   const myDataBase = await client.db('database').collection('users');
-
   // Be sure to change the title
+  passport.use(new LocalStrategy(
+    function (username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log('User ' + username + ' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
+
+
   app.route('/').get((req, res) => {
     //Change the response to render the Pug template
     res.render('pug', {
@@ -43,14 +53,11 @@ myDB(async client => {
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
-
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
       done(null, doc);
     });
   });
-
-
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
